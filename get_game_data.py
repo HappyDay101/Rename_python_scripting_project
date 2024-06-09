62a8101226be37a72630fa750b1a735f2b57dc29
Osama Suleiman <osama.suleiman97@gmail.com>
2024-06-09 22:32:14 +0900

    function made to find game paths and save

--- get_game_data.py
+++ get_game_data.py
@@ -4,6 +4,20 @@ import shutil
 from subprocess import PIPE, run
 import sys
 
+GAME_DIR_PATTERN = "game"
+
+def find_all_game_paths(source):
+    game_paths = []
+
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                pth = os.path.join(source, directory)
+                game_paths.append(path)
+
+
+        break
+
 def main(source, target):
     # Get current working directory
     cwd = os.getcwd()
